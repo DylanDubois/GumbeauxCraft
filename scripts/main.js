caf51ece7b1c66453619e70f9556b1c748a52738
DylanDubois <dubois.a.dylan@gmail.com>
2024-06-29 19:15:31 -0700

    blah

--- scripts/main.js
+++ scripts/main.js
@@ -1,21 +1,10 @@
-import { world, system, ItemStack } from "@minecraft/server";
+import { world, system } from "@minecraft/server";
 
-// Function to run when the world loads
-function onWorldLoad() {
-  world.sendMessage("Diamond Sword Giver pack loaded!");
-}
+system.runInterval(() => {
+  world.sendMessage("The script is running!");
+}, 200);
 
-// Run the onWorldLoad function after a short delay to ensure the world is fully loaded
-system.runTimeout(onWorldLoad, 20);
-
-world.events.beforeChat.subscribe((eventData) => {
-  if (eventData.message === "T") {
-    const players = world.getAllPlayers();
-    for (const player of players) {
-      const diamondSword = new ItemStack("minecraft:diamond_sword", 1);
-      player.getInventory().addItem(diamondSword);
-      player.sendMessage("You received a diamond sword!");
-    }
-    eventData.cancel = true;
-  }
+world.afterEvents.chatSend.subscribe((event) => {
+  const player = event.sender;
+  player.sendMessage("Hello! I received your chat message!");
 });
