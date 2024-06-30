1a39447470ed0d8edfed66b7a39a8766cfa20cee
DylanDubois <dubois.a.dylan@gmail.com>
2024-06-29 19:32:57 -0700

    blah

--- scripts/main.js
+++ scripts/main.js
@@ -1,15 +1,25 @@
 import { world, ItemStack } from "@minecraft/server";
 
-world.events.beforeChat.subscribe((eventData) => {
-  if (eventData.message === "T") {
-    const player = eventData.sender;
-    const diamondSword = new ItemStack("minecraft:diamond_sword", 1);
-    player.getComponent("inventory").container.addItem(diamondSword);
-    player.sendMessage("You received a diamond sword!");
-    eventData.cancel = true;
-  }
-});
+// Ensure the world and events are properly defined
+if (world && world.events && world.events.beforeChat) {
+  world.events.beforeChat.subscribe((eventData) => {
+    if (eventData.message === "T") {
+      const player = eventData.sender;
+      const inventory = player.getComponent("minecraft:inventory");
+      const container = inventory.container;
 
-world.events.worldInitialize.subscribe(() => {
-  world.sendMessage("Diamond Sword Giver pack loaded!");
-});
+      const diamondSword = new ItemStack("minecraft:diamond_sword", 1);
+      container.addItem(diamondSword);
+      player.sendMessage("You received a diamond sword!");
+      eventData.cancel = true;
+    }
+  });
+}
+
+if (world && world.events && world.events.worldInitialize) {
+  world.events.worldInitialize.subscribe(() => {
+    world.sendMessage("Diamond Sword Giver pack loaded!");
+  });
+} else {
+  console.log("worldInitialize event not available");
+}
