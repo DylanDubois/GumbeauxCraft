fbdc2069add285a203828947bee52890f1caeabe
DylanDubois <dubois.a.dylan@gmail.com>
2024-06-29 19:39:29 -0700

    blah

--- scripts/main.js
+++ scripts/main.js
@@ -1,6 +1,23 @@
-import * as mc from "@minecraft/server";
+import { world, ItemStack, system } from "@minecraft/server";
 
-mc.world.afterEvents.chatSend.subscribe((event) => {
-  const player = event.sender;
-  player.sendMessage("Hello! The script is working!");
+// Event handler for when the world initializes
+world.beforeEvents.worldInitialize.subscribe((eventData) => {
+  world.sendMessage("Diamond Sword Giver pack loaded!");
 });
+
+// Event handler for before a chat message is sent
+world.beforeEvents.chatSend.subscribe((eventData) => {
+  if (eventData.message === "T") {
+    const player = eventData.sender;
+    const inventory = player.getComponent("minecraft:inventory");
+    const container = inventory.container;
+
+    const diamondSword = new ItemStack("minecraft:diamond_sword", 1);
+    container.addItem(diamondSword);
+    player.sendMessage("You received a diamond sword!");
+    eventData.canceled = true;
+  }
+});
+
+// Start the system to ensure the script keeps running
+system.run(() => {});
